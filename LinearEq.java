464ba61022c9db6ddd8c9a51cc78be7c1b334ce5
Ido Admoni <ido.admoni@post.runi.ac.il>
2023-12-22 12:53:02 +0200

    HW1 is done

--- LinearEq.java
+++ LinearEq.java
@@ -9,9 +9,8 @@ public class LinearEq {
 		double a = Integer.parseInt(args[0]);
         double b = Integer.parseInt(args[1]);
 		double c = Integer.parseInt(args[2]);
-		double x= (c-b)/a;
+		double x= ((c-b)/a);
 		System.out.println(a + " * x" + " + " + b + " = " + c);
 		System.out.println("x = " + x);
     }
-}
 }
